ef4646274beb5be65ee238861523ced35309c4b4
mh <mh@immerda.ch>
2016-05-29 14:46:06 +0200

    cleanup for newer puppet support

--- manifests/web.pp
+++ manifests/web.pp
@@ -1,27 +1,32 @@
+# gitweb
 class git::web {
-  include git
+  include ::git
   package{'gitweb':
-    ensure => present,
+    ensure  => present,
     require => Package['git'],
   }
 
-  file{'/etc/gitweb.d':
-    ensure => directory,
-    owner => root, group => 0, mode => 0755;
-  }
-  file{'/etc/gitweb.conf':
-    source => [ "puppet:///modules/site_git/web/${::fqdn}/gitweb.conf",
-                "puppet:///modules/site_git/web/gitweb.conf",
-                "puppet:///modules/git/web/gitweb.conf" ],
-    require => Package['gitweb'],
-    owner => root, group => 0, mode => 0644;
-  }
-
-  file{"/var/www/git/robots.txt":
-    source => [ "puppet:///modules/site_git/web/${::fqdn}/robots.txt",
-                "puppet:///modules/site_git/web/robots.txt",
-                "puppet:///modules/git/web/robots.txt" ],
-    require => Package['gitweb'],
-    owner => root, group => 0, mode => 0644;
+  file{
+    '/etc/gitweb.d':
+      ensure => directory,
+      owner  => root,
+      group  => 0,
+      mode   => '0755';
+    '/etc/gitweb.conf':
+      source => [ "puppet:///modules/site_git/web/${::fqdn}/gitweb.conf",
+                  "puppet:///modules/site_git/web/gitweb.conf",
+                  "puppet:///modules/git/web/gitweb.conf" ],
+      require => Package['gitweb'],
+      owner   => root,
+      group   => 0,
+      mode    => '0644';
+    "/var/www/git/robots.txt":
+      source => [ "puppet:///modules/site_git/web/${::fqdn}/robots.txt",
+                  "puppet:///modules/site_git/web/robots.txt",
+                  "puppet:///modules/git/web/robots.txt" ],
+      require => Package['gitweb'],
+      owner   => root,
+      group   => 0,
+      mode    => '0644';
   }
 }
